0fa1aa906ea73416025276986ede219955f66335
Tyler Grass <twgrass12@gmail.com>
2019-08-05 12:57:46 -0700

    Add basic date utils

--- src/shared/util.js
+++ src/shared/util.js
@@ -1,3 +1,29 @@
-module.exports.snakeCase = function(str) {
-		return str.toLowerCase().replace(' ', '-');
-	}
+import dateFns from 'date-fns';
+
+export const snakeCase = function(str) {
+	return str.toLowerCase().replace(' ', '-');
+}
+
+export const formatDateStandard = function(date) {
+	return dateFns.format(date, 'YYYY-MM-DD');
+}
+
+export const formatDateNamedMonth = function(date) {
+	return dateFns.format(date, 'MMMM YYYY')
+}
+
+export const setToNextMonth = function(date) {
+	return dateFns.addMonths(date, 1);
+}
+
+export const setToPrevMonth = function(date) {
+	return dateFns.subMonths(date, 1);
+}
+
+export const setToNextDay = function(date) {
+	return dateFns.addDays(date, 1);
+}
+
+export const setToPrevDay = function(date) {
+	return dateFns.subDays(date, 1);
+}
