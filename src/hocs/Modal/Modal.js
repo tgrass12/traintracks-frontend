d61666e888080f90e67d0e6c392db2b7321912cb
Tyler Grass <twgrass12@gmail.com>
2019-08-19 13:50:06 -0700

    Add Modal HOC

---
+++ src/hocs/Modal/Modal.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactModal from 'react-modal';
+import '../../styles/Modal.scss';
+
+let Modal = (Component, isOpen = false, hideModal, props) => {
+	if (process.env.NODE_ENV !== 'test') {
+		ReactModal.setAppElement('.main');
+	}
+	else {
+		ReactModal.setAppElement(document.createElement('div'));
+	}
+
+	const shouldClose = () => {
+		if (hideModal) hideModal();
+		else {
+			isOpen = false;
+		}
+	}
+
+	return (
+		<ReactModal 
+			overlayClassName={'modal-overlay'}
+			className={'modal-content'} 
+			isOpen={isOpen}
+			onRequestClose={shouldClose}
+		>
+		{!!Component &&
+			<Component hideModal={hideModal} {...props}/>
+		}
+		</ReactModal>
+	);
+}
+
+export default Modal;
---
+++ src/hocs/Modal/Modal.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Modal from './Modal';
+
+it('should render an empty modal', () => {
+	const EmptyModal = Modal();
+	const wrapper = shallow(EmptyModal);
+	expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('should render a passed component', () => {
+	const MockComponent = () => (
+		<div> Hello World! </div>
+	);
+
+	const MockModal = Modal(MockComponent, true);
+	const wrapper = mount(MockModal);
+
+	expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it('should hide modal when hideFn called', () => {
+	let hideFn = jest.fn();
+	const MockComponent = () => (
+		<div> Hello World! </div>
+	);
+
+	const MockModal = Modal(MockComponent, false, hideFn);
+	const wrapper = shallow(MockModal);
+	wrapper.find('ModalPortal').props().onRequestClose();
+	expect(hideFn.mock.calls.length).toBe(1);
+});
