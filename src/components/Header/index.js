1850d39abeb816daf8a1822725dbfa7254077bfd
Tyler Grass <twgrass12@gmail.com>
2019-09-27 14:54:40 -0700

    Add basic auth with Auth0

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '../../Auth/auth0-wrapper';
 import '../../styles/Header.scss';
 
 let Header = () => {
+	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
 	return (
 		<div className="header">
 			<nav className="header-content">
 				<Link to="/" id="app-icon"> TrainTracks </Link>
-				<ul className="nav-items">
-					<li>
-						<Link to="/"> Dashboard </Link>
-					</li>
-					<li>
-						<Link to="/journal"> Journal </Link>
-					</li>
-					<li>
-						<Link to="/calendar"> Calendar </Link>
-					</li>
-				</ul>
+				{ isAuthenticated && 
+					<ul className="nav-items">
+						<li>
+							<Link to="/"> Dashboard </Link>
+						</li>
+						<li>
+							<Link to="/journal"> Journal </Link>
+						</li>
+						<li>
+							<Link to="/calendar"> Calendar </Link>
+						</li>
+					</ul>
+				}
+				<div className="auth-btn-container"> 
+					{ !isAuthenticated &&
+						<div>
+							<button 
+								id="register" 
+								onClick={() => loginWithRedirect()}
+							>
+								Register
+							</button>	
+							<button 
+								id="login"
+								onClick={() => loginWithRedirect()}
+							> 
+								Log in 
+							</button>
+						</div>
+					}
+					{ isAuthenticated && 
+						<button id="logout" onClick={() => logout()}> 
+							Sign out 
+						</button>
+					}
+				</div>
 			</nav>
 		</div>
 	)
--- src/components/Header/test/Header.test.js
+++ src/components/Header/test/Header.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import toJson from 'enzyme-to-json';
@@ -5,55 +6,124 @@ import { MemoryRouter } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import Header from '../';
 
-it('should render a navigation bar with links', () => {
-	const wrapper = renderer.create(
-		<MemoryRouter>
-			<Header/>
-		</MemoryRouter>
-	).toJSON();
-	expect(wrapper).toMatchSnapshot();
-});
+jest.mock('../../../Auth/auth0-wrapper', () => ({
+	useAuth0: jest.fn()
+}));
 
-it('should navigate to home page when logo is clicked', () => {
-	const wrapper = mount(
-		<MemoryRouter initialEntries={['/test']} keyLength={0}>
-			<Header/>
-		</MemoryRouter>
-	);
+import { useAuth0 } from '../../../Auth/auth0-wrapper';
 
-	wrapper.find('a#app-icon').simulate('click', { button: 0 });
-	expect(toJson(wrapper)).toMatchSnapshot();
-});
+describe('<Header/>', () => {
+	describe('User not logged in', () => {
+		const mockLogin = jest.fn();
+		beforeAll(() => {
+			useAuth0.mockImplementation(() => ({
+				isAuthenticated: false,
+				loginWithRedirect: mockLogin
+			}));
+		});
 
-it('should navigate to the dashboard when Dashboard is clicked', () => {
-	const wrapper = mount(
-		<MemoryRouter initialEntries={['/test']} keyLength={0}>
-			<Header/>
-		</MemoryRouter>
-	);
+		it('should render the nav bar with Login/Register links', () => {
+			const wrapper = renderer.create(
+				<MemoryRouter>
+					<Header/>
+				</MemoryRouter>
+			).toJSON();
+			expect(wrapper).toMatchSnapshot();
+		});
 
-	wrapper.find('li a[href="/"]').simulate('click', { button: 0 });
-	expect(toJson(wrapper)).toMatchSnapshot();
-});
+		it('should navigate to login page on login click', () => {
+			const wrapper = mount(
+				<MemoryRouter keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
 
-it('should navigate to the journal when Journal is clicked', () => {
-	const wrapper = mount(
-		<MemoryRouter keyLength={0}>
-			<Header/>
-		</MemoryRouter>
-	);
+			wrapper.find('button#login').simulate('click');
+			expect(mockLogin).toHaveBeenCalled();
+		});
 
-	wrapper.find('a[href="/journal"]').simulate('click', { button: 0 });
-	expect(toJson(wrapper)).toMatchSnapshot();
-});
+		it('should navigate to register on register click', () => {
+			const wrapper = mount(
+				<MemoryRouter keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+			wrapper.find('#register').simulate('click');
+			expect(mockLogin).toHaveBeenCalled();
+		});
+	});
 
-it('should navigate to the calendar when Calendar is clicked', () => {
-	const wrapper = mount(
-		<MemoryRouter keyLength={0}>
-			<Header/>
-		</MemoryRouter>
-	);
+	describe('User logged in', () => {
+		const mockLogout = jest.fn();
+		beforeAll(() => {
+			useAuth0.mockImplementation(() => ({
+				isAuthenticated: true,
+				logout: mockLogout
+			}));
+		});
 
-	wrapper.find('a[href="/calendar"]').simulate('click', { button: 0 });
-	expect(toJson(wrapper)).toMatchSnapshot();
+		it('should render a navigation bar with links for signed in user', () => {
+			const wrapper = renderer.create(
+				<MemoryRouter>
+					<Header/>
+				</MemoryRouter>
+			).toJSON();
+			expect(wrapper).toMatchSnapshot();
+		});
+
+		it('should navigate to home page when logo is clicked', () => {
+			const wrapper = mount(
+				<MemoryRouter initialEntries={['/test']} keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+
+			wrapper.find('a#app-icon').simulate('click', { button: 0 });
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
+
+		it('should navigate to the dashboard when Dashboard is clicked', () => {
+			const wrapper = mount(
+				<MemoryRouter initialEntries={['/test']} keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+
+			wrapper.find('li a[href="/"]').simulate('click', { button: 0 });
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
+
+		it('should navigate to the journal when Journal is clicked', () => {
+			const wrapper = mount(
+				<MemoryRouter keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+
+			wrapper.find('a[href="/journal"]').simulate('click', { button: 0 });
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
+
+		it('should navigate to the calendar when Calendar is clicked', () => {
+			const wrapper = mount(
+				<MemoryRouter keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+
+			wrapper.find('a[href="/calendar"]').simulate('click', { button: 0 });
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
+
+		it('should handle logout when logout is clicked', () => {
+			const wrapper = mount(
+				<MemoryRouter keyLength={0}>
+					<Header/>
+				</MemoryRouter>
+			);
+
+			wrapper.find('#logout').simulate('click');
+			expect(mockLogout).toHaveBeenCalled();			
+		});
+	});
 });
