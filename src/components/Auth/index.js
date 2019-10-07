1dfce554d02ca785a82c6f11761261ba2c4262f5
Tyler Grass <twgrass12@gmail.com>
2019-10-06 17:36:54 -0700

    Create Authentication form

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,16 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../../styles/Auth.scss';
 
 let Auth = ({isRegister, onAuth}) => {
+	const formHeaderText = isRegister ? 'Join Today' : 'Welcome Back';
+	const formButtonText = isRegister ? 'Register': 'Sign in';
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [isEmailValid, setIsEmailValid] = useState(!isRegister);
+	const [isPasswordValid, setIsPasswordValid] = useState(false);
+	const [isUsernameValid, setIsUsernameValid] = useState(false);
+
+	const isFormValid = isEmailValid && 
+		isPasswordValid && 
+		isUsernameValid;
+
+	//TODO: Set Form Validity checks/displays
+
+	useEffect(() => {
+		setEmail('');
+		setIsEmailValid(!isRegister);
+		setIsPasswordValid(validatePassword(password));
+		setIsUsernameValid(validateUsername(username));
+	},[isRegister]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (isFormValid) {
+			onAuth(username, password, email);
+		}
+	}
+
+	const validatePassword = (password) => {
+		return isRegister ? password.length > 6 : password.length > 0;
+	}
+
+	const validateUsername = (username) => {
+		return isRegister ? username.length > 2 : username.length > 0;
+	}
+
+	const validateEmail = (emailInput) => {
+		return isRegister ? emailInput.checkValidity() : true;
+	}
+
+	const handleChange = (e) => {
+		switch(e.target.name) {
+			case 'email':
+				setEmail(e.target.value);
+				setIsEmailValid(validateEmail(e.target));
+				return;
+			case 'username':
+				setUsername(e.target.value);
+				setIsUsernameValid(validateUsername(e.target.value));
+				return;
+			case 'password':
+				setPassword(e.target.value);
+				setIsPasswordValid(validatePassword(e.target.value));
+				return;
+			default:
+				return;
+		}
+	}
 
 	return (
-		<div>
-			{ !isRegister &&
-				<div> Login Component </div>
+		<form id="auth-form" onSubmit={handleSubmit}>
+			<h2>{formHeaderText}</h2>
+			{ isRegister && 
+				<div>
+					<label htmlFor='email'> Email </label>
+					<input 
+						id='auth-email'
+						name='email'
+						type='email'
+						value={email}
+						onChange={handleChange}
+					/>
+				</div>
 			}
-			{ isRegister &&
-				<div> Register Component </div>
-			}
-		</div>
+			<div>
+				<label htmlFor='username'> Username </label>
+				<input 
+					name='username'
+					value={username}
+					onChange={handleChange}
+				/>			
+			</div>
+			<div>
+				<label htmlFor='password'> Password </label>
+				<input 
+					name='password' 
+					type='password'
+					value={password}
+					onChange={handleChange}
+				/>			
+			</div>
+			<button type='submit'>
+				{formButtonText}
+			</button>
+		</form>
 	)
 }
 
--- src/components/Auth/test/Auth.test.js
+++ src/components/Auth/test/Auth.test.js
@@ -3,18 +3,24 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Auth from '../';
 
+//TODO: Figure out how to mock useState
 describe('<Auth />', () => {
+	describe('Log in', () => {
+		it('should render Login form', () => {
+			const wrapper = shallow(<Auth />);
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
 
-	it('should render Login if isRegister is false', () => {
-		const wrapper = shallow(<Auth />);
-		expect(toJson(wrapper)).toMatchSnapshot();
+		test.todo('should call onAuth with username, password with valid form');
 	});
 
-	it('should render Register if isRegister is true', () => {
-		const wrapper = shallow(<Auth isRegister/>);
-		expect(toJson(wrapper)).toMatchSnapshot();
+	describe('Register', () => {
+		it('should render Register form', () => {
+			const wrapper = shallow(<Auth isRegister/>);
+			expect(toJson(wrapper)).toMatchSnapshot();
+		});
+		test.todo('should validate username');
+		test.todo('should validate password');
+		test.todo('should call onAuth with username, email, password');
 	});
-
-	test.todo('should call onAuth when form submitted');
-	test.todo('should verify password requirements on registration');
 });
